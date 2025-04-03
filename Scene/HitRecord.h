480aaf8c14d3cd25bdd6e8eef112b9ff3ab1b983
yhomasti <yi.th@northeastern.edu>
2025-04-03 19:31:07 -0400

    Added Ray and HitRecord classes for 1.1

---
+++ Scene/HitRecord.h
@@ -0,0 +1,39 @@
+#ifndef HITRECORD_H
+#define HITRECORD_H
+
+#include <glm/glm.hpp>
+#include "Material.h"
+
+//Stores all information to determine closest object that was hit, along with information about that object
+//in order to calculate shading.
+class HitRecord {
+public:
+    //the ray parameter at the intersection
+    float t;  
+    //the coordinates of the intersection point                      
+    glm::vec3 point;
+    //surface normal at intersection                
+    glm::vec3 normal;    
+    //material at intersection          
+    util::Material material;        
+    bool hit;                       
+
+    //i have no idea if we need to use textures here but if we do we'll uncomment these lines
+    // glm::vec2 texCoords;
+    // Texture* texture;
+
+    HitRecord() {
+        t = std::numeric_limits<float>::max();
+        hit = false;
+    }
+
+    void update(float _t, const glm::vec3& _point, const glm::vec3& _normal, const util::Material& _material) {
+        t = _t;
+        point = _point;
+        normal = glm::normalize(_normal);
+        material = _material;
+        hit = true;
+    }
+};
+
+#endif
---
+++ Scene/Ray.h
@@ -0,0 +1,20 @@
+#ifndef _RAY_H_
+#define _RAY_H_
+
+#include <glm/glm.hpp>
+
+//Represents a 3D ray, containing a starting 3D point and a direction as a 3D vector
+class Ray {
+public:
+    glm::vec3 origin;
+    glm::vec3 direction; 
+
+    Ray(const glm::vec3& o, const glm::vec3& d)
+        : origin(o), direction(glm::normalize(d)) {}
+
+    glm::vec3 pointAt(float t) const {
+        return origin + t * direction;
+    }
+};
+
+#endif
